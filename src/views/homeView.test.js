af355db644fdfe447e0d941011af7f9f262c75e1
ananafrida <anan.afrida.yes@gmail.com>
2023-12-08 09:23:36 -0500

    add comments to code (issue #32)

--- src/views/homeView.test.js
+++ src/views/homeView.test.js
@@ -1,30 +1,27 @@
-//this is for 4.2(b)
+// Importing necessary dependencies from React, testing-library, and react-router-dom
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
+
+// Importing the component to be tested (HomeView in this case)
 import HomeView from './homeView';
 
+// Test case: checking navigation to "/create" when the "Create" link is clicked
 test('navigates to "/create" when the "Create" link is clicked', () => {
+  // Rendering the HomeView component within a Router
   render(
     <Router>
       <HomeView />
     </Router>
   );
 
+  // Retrieving the "Create" link from the rendered component
   const createLink = screen.getByText('Create');
+  
+  // Simulating a click on the "Create" link
   fireEvent.click(createLink);
 
-  // After clicking the link, you can assert that the navigation has occurred
+  // After clicking the link, asserting that the navigation has occurred
+  // by checking the current pathname in the window location
   expect(window.location.pathname).toBe('/create');
 });
-
-// test('renders HomeView component', () => {
-//   render(
-//     <Router>
-//       <HomeView />
-//     </Router>
-//   );
-
-//   // You can add more specific assertions based on your UI
-//   expect(screen.getByText('Search by Artist')).toBeInTheDocument();
-// });
